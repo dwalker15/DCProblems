fc4fc12f6d236de2c656b9f1aa1fe697fecfb28d
Davontae Walker <davontaewalker15@gmail.com>
2018-12-02 22:31:51 -0600

    Daily Coding Problem #108

---
+++ ShiftedEquals/ShiftedEquals/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShiftedEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string A = "abcde";
+            string B = "cdeab";
+            Debug.Assert(IsShiftedEqual(A, B));
+
+            A = "abc";
+            B = "acb";
+            Debug.Assert(!IsShiftedEqual(A, B));
+
+            A = "abaabbabcabdabeabf";
+            B = "bbabcabdabeabfabaa";
+            Debug.Assert(IsShiftedEqual(A, B));
+        }
+
+        public static bool IsShiftedEqual(string A, string B)
+        {
+            List<int> indices = new List<int>();
+            int index = 0;
+            while (index != -1)
+            {
+                index = B.IndexOf(A.ElementAt(0), index + 1);
+                if (index != -1)
+                {
+                    indices.Add(index);
+                }
+            }
+            
+            foreach (int j in indices)
+            {
+                int i = j;
+                bool match = true;
+                for (int k = 1; k < A.Count(); k++)
+                {
+                    if (i == A.Count() - 1)
+                    {
+                        i = -1;
+                    }
+                    if (A.ElementAt(k) != B.ElementAt(++i))
+                    {
+                        match = false;
+                    }
+                    if (!match)
+                    {
+                        break;
+                    }
+                }
+                if (match)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
